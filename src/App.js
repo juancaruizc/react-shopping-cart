b42d086138085ed093bbbb655a7b189e5d12b1f4
juancaruizc <juancruizcruz@gmail.com>
2020-12-10 16:00:24 -0700

    completed stretch

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ function App() {
 		setCart([...cart, item])
 	};
 
-	// const removeItem = (id) => {
-	// 	let newCart = cart.filter((item) => item.id !== id);
-	// 	setCart(newCart);
-	//   };
+	const removeItem = (id) => {
+		let newCart = cart.filter((item) => item.id !== id);
+		setCart(newCart);
+	  };
 
 	return (
 		<div className="App">
 			<ProductContext.Provider value = {{products, addItem}}>
-				<CartContext.Provider value = {{cart}}>
+				<CartContext.Provider value = {{cart, removeItem}}>
 				<Navigation />
 
 				{/* Routes */}
